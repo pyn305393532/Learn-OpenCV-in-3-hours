f3aae1bf777c437a8bab9ccca2bf05836313da08
pyn305393532 <55179720+pyn305393532@users.noreply.github.com>
2020-06-22 10:40:18 +0800

    Update chapter8.py

--- chapter8.py
+++ chapter8.py
@@ -33,7 +33,7 @@ def stackImages(scale,imgArray):
     return ver
 
 def getContours(img):
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    ret, contours, hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
         print(area)
